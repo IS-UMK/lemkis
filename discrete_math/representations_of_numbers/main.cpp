7ef3d867ad761f49e5a8e18c8bde82511dc33442
mgolaszewski5623 <95693691+mgolaszewski5623@users.noreply.github.com>
2024-03-20 19:35:21 +0100

    project 'Expansion'

---
+++ discrete_math/representations_of_numbers/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "representation.hpp"
+
+int main() {
+    using namespace representation_namespace;
+
+    representation representation_class;
+
+    fraction<int> my_fraction = representation_class.create_fraction(183, 23);
+    expansion<16> my_expansion = representation_class.create_expansion<16>(16);
+
+    std::cout << "\nLicznik:\t" << my_fraction.numerator;
+    std::cout << "\nMianownik:\t" << my_fraction.denominator;
+    std::cout << "\nBaza:\t" << my_expansion.form;
+    std::cout << "\nWynik zamiany:\t" << representation_class.expansion_in_base(my_fraction, my_expansion);
+
+    return 0;
+}
