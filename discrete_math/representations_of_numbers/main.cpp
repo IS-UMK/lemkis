adc9b71882671bbf3c144863b571eb0085f0db28
mgolaszewski5623 <95693691+mgolaszewski5623@users.noreply.github.com>
2024-03-22 20:01:56 +0100

    Add files via upload

---
+++ discrete_math/representations_of_numbers/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "representation.hpp"
+#include <print>
+
+int main() {
+    using namespace representation;
+
+    fraction<int> my_fraction(2, 45);
+    expansion<10> my_expansion = expansion<10>();
+
+    std::print("\nLicznik:\t{}", my_fraction.numerator);
+    std::print("\nMianownik:\t{}", my_fraction.denominator);
+    std::print("\nBaza:\t{}", my_expansion.base_v);
+
+    expand(my_fraction, my_expansion);
+    std::print("\nWhole\t {}", my_expansion.whole);
+    std::print("\nFriactial\t {}", my_expansion.fractial);
+    std::print("\nPeriod\t {}", my_expansion.period);
+    std::print("\n{}.{}({})", my_expansion.whole, my_expansion.fractial, my_expansion.period);
+
+    return 0;
+}
