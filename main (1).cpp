e8d43f3dccba9839c24cfec6240d3f587c4ff427
Piotr G. 322937 <150648985+Hahatek@users.noreply.github.com>
2024-05-09 08:59:16 +0200

    main.cpp

--- main (1).cpp
+++ main (1).cpp
@@ -2,5 +2,8 @@
 #include "polynomial.hpp"
 
 int main() {
-	using namespace polynomial;
+	polynomial::polynomial<double> p1{{1, 2, 3}}; // x^2 + 2x + 3
+    	polynomial::polynomial<double> p2{{1, 1}};    // x + 1
+	
+   	return 0;
 }
