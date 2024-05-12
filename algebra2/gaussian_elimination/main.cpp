1cd7906cb2d66b5fe1c9db34812c698d8e4b5718
andriybriliant <150779350+andriybriliant@users.noreply.github.com>
2024-05-12 23:44:11 +0200

    Deleted algebra2/gaussian_elimination/main.cpp

--- algebra2/gaussian_elimination/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "gaussian_elimination.h"
-
-
-int main() {
-    gaussian_elimination_examples<int, std::layout_right>();
-    gaussian_elimination_examples_fractions<int, std::layout_right>();
-    return 0;
-}
