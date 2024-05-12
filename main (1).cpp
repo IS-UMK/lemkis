8e2369b0c5809b58a3d7becb806b7391c36d9242
Piotr G. 322937 <150648985+Hahatek@users.noreply.github.com>
2024-05-12 21:32:01 +0200

    Update and rename main (1).cpp to main.cpp

--- main (1).cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "polynomial.hpp"
-
-int main() {
-	polynomial::polynomial<double> p1{{1, 2, 3}}; // x^2 + 2x + 3
-    	polynomial::polynomial<double> p2{{1, 1}};    // x + 1
-	
-   	return 0;
-}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "polynomial.hpp"
+
+int main() {
+   	using namespace polynomial;
+   	test();
+   	return 0;
+}
