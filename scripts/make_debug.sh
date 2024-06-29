02f1007f067d1fdb60ee24cd328019181fab9ec0
lemkis <159472816+lemkis@users.noreply.github.com>
2024-06-29 10:04:09 +0200

    Update make_debug.sh

--- scripts/make_debug.sh
+++ scripts/make_debug.sh
@@ -1,1 +1,1 @@
-CXX=g++ CC=gcc cmake -B./build/debug -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug . && cd build/debug && ninja 
+CXX=clang++-18 CC=clang-18 cmake -B./build/debug -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug . && cd build/debug && ninja 
