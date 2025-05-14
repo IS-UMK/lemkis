5948aaade1f4a14f72bd08ffec5228019f818703
lemkis <mlemanczyk@umk.pl>
2025-05-14 13:07:15 +0200

    added experimental clang flag to support jthread

--- scripts/make_debug.sh
+++ scripts/make_debug.sh
@@ -1,1 +1,1 @@
-CXX=clang++-18 CC=clang-18 cmake -B./build/debug -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug . && cd build/debug && ninja 
+CXX=clang++-19 CC=clang-19 cmake -B./build/debug -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug . && cd build/debug && ninja 
