9968d2210f2af1489e74c604c92017a0f512ba50
wapierz <scippiafri@protonmail.com>
2025-05-19 18:20:23 +0200

    actions now should work

--- scripts/make_debug.sh
+++ scripts/make_debug.sh
@@ -1,5 +1,5 @@
 CXX=clang++-19 CC=clang-19 cmake -B./build/debug -GNinja \
--DCMAKE_EXPORT_COMPILE_COMMANDS=ON\
--DCMAKE_BUILD_TYPE=Debug\
+ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON\
+ -DCMAKE_BUILD_TYPE=Debug\
  . \
 && cd build/debug && ninja 
