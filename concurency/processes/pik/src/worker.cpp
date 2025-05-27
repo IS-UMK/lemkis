e76a275f629b054aaa4753e2c0a69b7a54da224a
BartoszSykutera <59446673+BartoszSykutera@users.noreply.github.com>
2025-05-28 01:03:34 +0200

    Update worker.cpp

--- concurency/processes/pik/src/worker.cpp
+++ concurency/processes/pik/src/worker.cpp
@@ -1,29 +1,43 @@
 #include "worker.h"
-#include "prelude.h"
+
 #include "pipes.h"
+#include "prelude.h"
 #include "semaphores.h"
 #include "unit.h"
 
+void consume(bool& start_first, int id, unit rcv) {
+    std::println(
+        "[Worker {}] Received and consumed unit of type {} produced by "
+        "Worker {}",
+        id,
+        unit_type_to_string(rcv.type),
+        rcv.producer_id);
+    usleep(sleep_long);
+    start_first = true;
+}
+
+void sent_unit(int id) {
+    sem_wait(ready[id]);
+    const unit u = produce(id);
+    write_unit(wt_helper[id][write_fd], u);
+    std::println("[Worker {}] Sent unit to Helper", id);
+    usleep(sleep_long);
+}
+
+void process_loop(bool& start_first, int id) {
+    unit rcv;
+    if (start_first) { sent_unit(id); }
+
+    if (read_unit(ht_worker[id][read_fd], rcv)) {
+        consume(start_first, id, rcv);
+    }
+    usleep(sleep_short);
+}
+
 void worker_process(int id) {
     bool start_first = id % number_two != 0;
-    unit rcv;
     close(wt_helper[id][read_fd]);
     close(ht_worker[id][write_fd]);
     srand(time(nullptr) + getpid());
-    while (true) {
-        if (start_first) {
-            sem_wait(ready[id]);
-            const unit u = produce(id);
-            write_unit(wt_helper[id][write_fd], u);
-            std::println("[Worker {}] Sent unit to Helper", id);
-            usleep(sleep_long);
-        }
-
-        if (read_unit(ht_worker[id][read_fd], rcv)) {
-            std::println("[Worker {}] Received and consumed unit of type {} produced by Worker {}", id, unit_type_to_string(rcv.type), rcv.producer_id);
-            usleep(sleep_long);
-            start_first = true;
-        }
-        usleep(sleep_short);
-    }
+    while (true) { process_loop(start_first, id); }
 }
