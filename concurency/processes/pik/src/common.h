d3cc566cb1044294872e204dbfec3d36be7ce8e3
BartoszSykutera <59446673+BartoszSykutera@users.noreply.github.com>
2025-05-28 00:59:45 +0200

    Update common.h

--- concurency/processes/pik/src/common.h
+++ concurency/processes/pik/src/common.h
@@ -4,8 +4,13 @@
 constexpr int number_of_workers = 5;
 constexpr int sleep_short = 5000;
 constexpr int sleep_long = 100000;
+constexpr int chmod = 0666;
+constexpr int sem_open_value = 1;
+constexpr int number_of_all_processe = 2 * number_of_workers;
+constexpr int pipes_ends_number = 2;
 constexpr int number_two = 2;
 constexpr int read_fd = 0;
 constexpr int write_fd = 1;
+constexpr int error_value_byte = -1;
 
 #endif
