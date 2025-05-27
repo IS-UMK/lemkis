f31ce1d535f5aeb00db40436644a92effb532521
BartoszSykutera <59446673+BartoszSykutera@users.noreply.github.com>
2025-05-28 01:02:13 +0200

    Update semaphores.cpp

--- concurency/processes/pik/src/semaphores.cpp
+++ concurency/processes/pik/src/semaphores.cpp
@@ -6,10 +6,10 @@ void init_semaphores() {
     for (int i = 0; i < number_of_workers; ++i) {
         const std::string sem_name = "/ready" + std::to_string(i);
         sem_unlink(sem_name.c_str());
-        ready[i] = sem_open(sem_name.c_str(), O_CREAT, 0666, 1);
+        ready[i] = sem_open(sem_name.c_str(), O_CREAT, chmod, sem_open_value);
         if (ready[i] == SEM_FAILED) {
             perror("sem_open");
-            exit(1);
+            exit(sem_open_value);
         }
     }
 }
