d7f16a52674752260eac34d8cc5ea5ef07a90539
Julian Świerczyński <icytwv@gmail.com>
2025-05-28 13:09:07 +0200

    build error

--- concurency/student_projects/openmp/queue/src/include/queue_test.hpp
+++ concurency/student_projects/openmp/queue/src/include/queue_test.hpp
@@ -36,4 +36,4 @@ auto static run_benchmark_suite(const std::string& queue_name,
                          BenchmarkFunc benchmark_function) -> benchmark_result;
 
 // Porównanie pełne — uruchamia wszystkie kombinacje kolejek i metod
-auto static run_full_comparison(int num_producers, int num_consumers) -> void;
+auto run_full_comparison(int num_producers, int num_consumers) -> void;
