e75b6de0fea557324427147dc1cf6a488da906be
BartoszSykutera <59446673+BartoszSykutera@users.noreply.github.com>
2025-06-09 00:36:22 +0200

    Update worker.h

--- concurency/processes/pik/src/worker.h
+++ concurency/processes/pik/src/worker.h
@@ -1,6 +1,30 @@
 #ifndef WORKER_H
 #define WORKER_H
 
-void worker_process(int id);
+#include "unit.h"
+
+class Worker {
+  public:
+    explicit Worker(int id);
+    void run();
+
+  private:
+    int id;
+    bool shouldSendSignal;
+    message received;
+
+    void close_unused_pipes() const;
+    void processLoop();
+
+    void sendUnitToHelper();
+    void sendSignalToHelper();
+    void receiveFromHelper();
+
+    bool handleReceivedMessage(const message& u);
+
+    void logUnitSent() const;
+    void logSignalSent() const;
+    void logMessageReceived(const message& u) const;
+};
 
 #endif
