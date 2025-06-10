715c0c32d2dedb37b43e3f736e6d32d1ba477936
Piotr Piekański <piotr.piekan@gmail.com>
2025-06-10 22:24:58 +0200

    uint8_t return codes

--- concurency/student_projects/StackAndQueue/src/main.cpp
+++ concurency/student_projects/StackAndQueue/src/main.cpp
@@ -5,19 +5,18 @@
 
 namespace {
     constexpr std::string_view file_name = "results.csv";
-}
+
+    enum std::uint8_t codes{success, error};
+}  // namespace
 
 auto main() noexcept -> int {
-    constexpr int success_code = 0;
-    constexpr int error_code = 1;
-
     try {
         benchmark_script::run_all_configurations(file_name);
-        return success_code;
+        return SUCCESS;
     } catch (const std::exception& e) {
         std::fputs("Unhandled std::exception: ", stdout);
         std::fputs(e.what(), stdout);
         std::fputs("\n", stdout);
     } catch (...) { std::fputs("Unhandled unknown exception\n", stdout); }
-    return error_code;
+    return ERROR;
 }
