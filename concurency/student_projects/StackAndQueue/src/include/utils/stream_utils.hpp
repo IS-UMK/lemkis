c22780c3d21cf3fcc623bd95766c7ec20cb3f39e
Piotr Piekański <piotr.piekan@gmail.com>
2025-06-11 10:34:29 +0200

    comment for streamsize

--- concurency/student_projects/StackAndQueue/src/include/utils/stream_utils.hpp
+++ concurency/student_projects/StackAndQueue/src/include/utils/stream_utils.hpp
@@ -1,8 +1,22 @@
+/**
+ * @file stream_utils.hpp
+ * @brief Contains utility functions for stream operations.
+ */
+
 #pragma once
 
 #include <cstddef>
 #include <ostream>
 
+/**
+ * @brief Safely converts a std::size_t value to std::streamsize.
+ *
+ * This is used when writing binary or formatted data to output streams,
+ * ensuring the size passed is of correct type.
+ *
+ * @param size The size value to convert.
+ * @return The size as std::streamsize.
+ */
 inline auto to_streamsize(std::size_t size) -> std::streamsize {
     return static_cast<std::streamsize>(size);
 }
