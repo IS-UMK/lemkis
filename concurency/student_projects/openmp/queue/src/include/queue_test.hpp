96f57b20c13a80429badbfea1e69aff762950666
Piotr Piekański <98901251+PiotrPiekan@users.noreply.github.com>
2025-06-11 11:23:08 +0200

    !!! DELETED OPENMP PROJECT !!!
    This project was preventing our PR from passing clang-format/tidy.
    
    The corrected project should already be at the repo, but we can't sync without wasting a lot of time that we don't have.

--- concurency/student_projects/openmp/queue/src/include/queue_test.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Deklaracje funkcji testujących
-void run_sequential_ompqueue_test(int producers, int consumers);
-void run_parallel_ompqueue_test(int producers, int consumers);
-void run_jthread_concurrentqueue_test(int producers, int consumers);
